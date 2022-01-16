dab0784d54d86c1950da36291669404899e4e279
sanghun0724 <sanghun0724@naver.com>
2022-01-16 23:34:42 +0900

    storage

---
+++ GeekBlog/GeekBlog/Resources/StorageManager.swift
@@ -0,0 +1,15 @@
+//
+//  storageManager.swift
+//  GeekBlog
+//
+//  Created by sangheon on 2022/01/16.
+//
+
+import FirebaseStorage
+
+public class StorageManager {
+    static let shared = StorageManager()
+    
+    // MARK: - Public
+    
+}
