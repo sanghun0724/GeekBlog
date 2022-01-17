0dac756d4aadd890b1445f76a15942c4cfac8711
sanghun0724 <sanghun0724@naver.com>
2022-01-17 15:45:56 +0900

    add Extension

--- GeekBlog/GeekBlog/Controllers/Others/OnBoarding/LoginViewController.swift
+++ GeekBlog/GeekBlog/Controllers/Others/OnBoarding/LoginViewController.swift
@@ -8,11 +8,72 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    private let usernameEmailField:UITextField = {
+        return UITextField()
+    }()
+    
+    private let passwordField:UITextField = {
+        let field = UITextField()
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let loginButton:UIButton = {
+        return UIButton()
+    }()
+    
+    private let termsButton:UIButton = {
+        return UIButton()
+    }()
+    
+    private let privacyButton:UIButton = {
+        return UIButton()
+    }()
+    
+    private let createAccountButton:UIButton = {
+        return UIButton()
+    }()
+    
+    private let headerView:UIView = {
+        return UIView()
+    }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .red
+        addSubViews()
+        view.backgroundColor = .systemBackground
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        //Asign Frames
+        headerView.frame = CGRect(x: 0,
+                                  y: view.safeAreaInsets.top,
+                                  width: view.width,
+                                  height: 200)
+    }
+    
+    
+    
+    func addSubViews() {
+        view.addSubview(usernameEmailField)
+        view.addSubview(passwordField)
+        view.addSubview(loginButton)
+        view.addSubview(termsButton)
+        view.addSubview(privacyButton)
+        view.addSubview(createAccountButton)
+        view.addSubview(headerView)
+    }
+    
+    @objc private func didTabLoginButton() {}
+    
+    @objc private func didTabTermsButton() {}
+    
+    @objc private func didTabPrivacyButton() {}
+    
+    @objc private func didTabCreateAccountButton() {}
+    
 }
