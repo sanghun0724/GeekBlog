b5ca832e73d8afcf3424f4be07a37c7b5117fc87
sanghun0724 <sanghun0724@naver.com>
2022-01-17 16:29:54 +0900

    feat LoginViewController

--- GeekBlog/GeekBlog/Controllers/Others/OnBoarding/LoginViewController.swift
+++ GeekBlog/GeekBlog/Controllers/Others/OnBoarding/LoginViewController.swift
@@ -23,6 +23,8 @@ class LoginViewController: UIViewController {
         field.autocorrectionType = .no
         field.layer.cornerRadius = Constants.cornerRadius
         field.backgroundColor = .secondarySystemBackground
+        field.layer.borderWidth = 1.0
+        field.layer.borderColor = UIColor.secondaryLabel.cgColor
         return field
     }()
     
@@ -30,12 +32,15 @@ class LoginViewController: UIViewController {
         let field = UITextField()
         field.isSecureTextEntry = true
         field.placeholder = "Username or Email"
-        field.returnKeyType = .next
+        field.returnKeyType = .continue
         field.leftViewMode = .always
         field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
         field.autocapitalizationType = .none
         field.autocorrectionType = .no
         field.layer.cornerRadius = Constants.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
+        field.layer.borderWidth = 1.0
+        field.layer.borderColor = UIColor.secondaryLabel.cgColor
         return field
     }()
     
@@ -50,24 +55,31 @@ class LoginViewController: UIViewController {
     }()
     
     private let termsButton:UIButton = {
-        return UIButton()
+        let button = UIButton()
+        button.setTitle("Terms of Service", for: .normal)
+        button.setTitleColor(.secondaryLabel, for: .normal)
+        return button
     }()
     
     private let privacyButton:UIButton = {
-        return UIButton()
+        let button = UIButton()
+        button.setTitle("Privacy Policy", for: .normal)
+        button.setTitleColor(.secondaryLabel, for: .normal)
+        return button
     }()
     
     private let createAccountButton:UIButton = {
         let button = UIButton()
         button.setTitleColor(.label, for: .normal)
         button.setTitle("New User? Creat an Account", for: .normal)
-        return UIButton()
+        return button
     }()
     
     private let headerView:UIView = {
         let header = UIView()
         header.clipsToBounds = true
-        header.backgroundColor = .red
+        let backgroundImageView = UIImageView(image: UIImage(named: "gradient"))
+        header.addSubview(backgroundImageView)
         return header
     }()
     
@@ -75,7 +87,19 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loginButton.addTarget(self, action: #selector(didTabLoginButton), for: .touchUpInside)
+        createAccountButton.addTarget(self, action: #selector(didTabCreateAccountButton), for: .touchUpInside)
+        termsButton.addTarget(self, action: #selector(didTabTermsButton), for: .touchUpInside)
+        privacyButton.addTarget(self, action: #selector(didTabPrivacyButton), for: .touchUpInside)
+        
+        
+        
+        usernameEmailField.delegate = self
+        passwordField.delegate = self
+        
         addSubViews()
+        
         view.backgroundColor = .systemBackground
     }
 
@@ -83,11 +107,52 @@ class LoginViewController: UIViewController {
         super.viewDidLayoutSubviews()
         //Asign Frames
         headerView.frame = CGRect(x: 0,
-                                  y: view.safeAreaInsets.top,
+                                  y: 0.0,
                                   width: view.width,
                                   height: view.height / 3)
+        usernameEmailField.frame = CGRect(x: 25,
+                                          y: headerView.bottom + 10,
+                                  width: view.width - 50,
+                                          height: 52.0)
+        passwordField.frame = CGRect(x: 25,
+                                     y: usernameEmailField.bottom + 10,
+                                  width: view.width - 50,
+                                     height: 52.0)
+        loginButton.frame = CGRect(x: 25,
+                                     y: passwordField.bottom + 10,
+                                  width: view.width - 50,
+                                     height: 52.0)
+        createAccountButton.frame = CGRect(x: 25,
+                                     y: loginButton.bottom + 10,
+                                  width: view.width - 50,
+                                     height: 52.0)
+        
+        termsButton.frame = CGRect(x: 10, y: view.height-view.safeAreaInsets.bottom-100, width: view.width-20, height: 50)
+        privacyButton.frame = CGRect(x: 10, y: view.height-view.safeAreaInsets.bottom-50, width: view.width-20, height: 50)
+        
+        
+        configureHeaderView()
     }
     
+    private func configureHeaderView() {
+        guard headerView.subviews.count == 1 else {
+            return
+        }
+        
+        guard let backgroundView = headerView.subviews.first as? UIView else {
+            return
+        }
+        backgroundView.frame = headerView.bounds
+        //Add instagram logo
+        let imageView = UIImageView(image:UIImage(named: "text"))
+        headerView.addSubview(imageView)
+        imageView.contentMode = .scaleAspectFit
+        imageView.frame = CGRect(x: headerView.width/4.0,
+                                 y: view.safeAreaInsets.top,
+                                 width: headerView.width/2.0,
+                                 height: headerView.height - view.safeAreaInsets.top)
+        
+    }
     
     
     func addSubViews() {
@@ -108,4 +173,15 @@ class LoginViewController: UIViewController {
     
     @objc private func didTabCreateAccountButton() {}
     
+}
+
+extension LoginViewController:UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == usernameEmailField {
+            passwordField.becomeFirstResponder()
+        } else if textField == passwordField {
+            didTabLoginButton()
+        }
+        return true
+    }
 }
