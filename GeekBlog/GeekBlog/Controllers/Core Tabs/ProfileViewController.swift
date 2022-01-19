0b2515fa288c8f3ff1253d086afa031dd158e244
sanghun0724 <sanghun0724@naver.com>
2022-01-19 22:35:48 +0900

    add Models

--- GeekBlog/GeekBlog/Controllers/Core Tabs/ProfileViewController.swift
+++ GeekBlog/GeekBlog/Controllers/Core Tabs/ProfileViewController.swift
@@ -61,13 +61,20 @@ class ProfileViewController: UIViewController {
 }
 
 extension ProfileViewController:UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0 {
+            return 0
+        }
         return 30
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier, for: indexPath) as! PhotoCollectionViewCell
-        cell.backgroundColor = .systemBlue
+        cell.configure(debug: "test")
         return cell
         
     }
@@ -77,4 +84,29 @@ extension ProfileViewController:UICollectionViewDelegate,UICollectionViewDataSou
         
     }
     
-}
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard kind == UICollectionView.elementKindSectionHeader else {
+            // footer
+            return UICollectionReusableView()
+        }
+        
+        if indexPath.section == 1 {
+            //tabs header
+            let tabControlHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier:ProfileTabsCollectionReusableView.identifier, for: indexPath) as! ProfileTabsCollectionReusableView
+            
+            return tabControlHeader
+        }
+        let profileHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: ProfileInfoHeaderCollectionReusableView.identifier, for: indexPath) as! ProfileInfoHeaderCollectionReusableView
+        
+        return profileHeader
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        if section == 0 {
+            return CGSize(width: collectionView.width, height: collectionView.height/3)
+        }
+        //Size info section tabs
+        return CGSize(width: collectionView.width, height: 65)
+    }
+   }
---
+++ GeekBlog/GeekBlog/Models/Model.swift
@@ -0,0 +1,63 @@
+//
+//  Model.swift
+//  GeekBlog
+//
+//  Created by sangheon on 2022/01/19.
+//
+
+import Foundation
+
+enum Gender {
+    case male, female, other
+}
+
+struct User {
+    let username:String
+    let bio:String
+    let name:(first:String,last:String)
+    let birthDate:Date
+    let gender:Gender
+    let count:UserCount
+    let joinDate:Date
+}
+
+struct UserCount {
+    let followers:Int
+    let following:Int
+    let posts:Int
+}
+
+
+public enum UserPostType {
+    case photo,video
+}
+/// represent a user post
+public struct UserPost {
+    let identifier:String
+    let postType:UserPostType
+    let thumbnailImage:URL
+    let postURL:URL //either video url or full resolution photo
+    let caption:String?
+    let likeCount:[PostLike]
+    let comments:[PostComment]
+    let createdDate:Date
+    let taggedUsers:[String]
+}
+struct PostLike {
+    let usename:String
+    let postIdentifier:String
+}
+
+struct CommentLike {
+    let username:String
+    let commentIdentifier:String
+}
+
+struct PostComment {
+    let identifier:String
+    let username:String
+    let text:String
+    let createdDate:Date
+    let likes:[CommentLike]
+}
+
