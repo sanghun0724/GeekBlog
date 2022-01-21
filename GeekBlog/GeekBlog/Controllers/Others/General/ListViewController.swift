7939e4df6a5fc2847d0c9382f5a45bac8e3977aa
sanghun0724 <sanghun0724@naver.com>
2022-01-21 12:49:58 +0900

    add ListVIewController

--- GeekBlog/GeekBlog/Controllers/Others/General/ListViewController.swift
+++ GeekBlog/GeekBlog/Controllers/Others/General/ListViewController.swift
@@ -13,7 +13,7 @@ class ListViewController: UIViewController {
     
     private let tableView:UITableView = {
         let tableView = UITableView()
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.register(UserFollowTableViewCell.self, forCellReuseIdentifier: UserFollowTableViewCell.identifier)
         return tableView
     }()
     
@@ -51,8 +51,8 @@ extension ListViewController:UITableViewDelegate,UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",for: indexPath)
-        cell.textLabel?.text = data[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: UserFollowTableViewCell.identifier,for: indexPath) as! UserFollowTableViewCell
+        cell.configure(with: "")
         return cell
     }
     
--- GeekBlog/GeekBlog/Views/Cells/UserFollowTableViewCell.swift
+++ GeekBlog/GeekBlog/Views/Cells/UserFollowTableViewCell.swift
@@ -7,17 +7,68 @@
 
 import UIKit
 
+protocol UserFollowTableViewCellDelegate:AnyObject {
+    func didTapFollowUnfollowButton(model:String)
+}
+
 class UserFollowTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    static let identifier = "UserFollowTableViewCell"
+    
+    weak var delegate:UserFollowTableViewCellDelegate?
+    
+    private let profileImageView:UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let nameLabel:UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.font = .systemFont(ofSize: 17, weight: .bold)
+        return label
+    }()
+    
+    private let usernameLabel:UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        return label
+    }()
+    
+    private let followButton:UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.clipsToBounds = true
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(usernameLabel)
+        contentView.addSubview(profileImageView)
+        contentView.addSubview(followButton)
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
     }
-
+    
+    public func configure(with model:String) {
+        
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        profileImageView.image = nil
+        nameLabel.text = nil
+        usernameLabel.text = nil
+        followButton.setTitle(nil, for: .normal)
+        followButton.layer.borderWidth = 0
+        followButton.backgroundColor = nil
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
