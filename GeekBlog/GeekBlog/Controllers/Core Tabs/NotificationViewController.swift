55adbe129d697750a153b23f0a66a331292bd1b7
sanghun0724 <sanghun0724@naver.com>
2022-01-23 17:56:37 +0900

    Error: cell update Error -> solvve (iflayoutNeedit)

--- GeekBlog/GeekBlog/Controllers/Core Tabs/NotificationViewController.swift
+++ GeekBlog/GeekBlog/Controllers/Core Tabs/NotificationViewController.swift
@@ -19,7 +19,7 @@ struct UserNotification {
 }
 
 final class NotificationViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-
+    
     private let tableview:UITableView = {
         let tableView = UITableView()
         tableView.isHidden = false
@@ -37,10 +37,13 @@ final class NotificationViewController: UIViewController,UITableViewDelegate,UIT
     
     private lazy var noNotificationView = NoNotificationView()
     
+    private var models = [UserNotification]()
+    
     //MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchNotifications()
         navigationItem.title = "title"
         view.backgroundColor = .systemBackground
         view.addSubview(spinner)
@@ -57,6 +60,31 @@ final class NotificationViewController: UIViewController,UITableViewDelegate,UIT
         spinner.center = view.center
     }
     
+    private func fetchNotifications() {
+        for x in 0..<100 {
+            let post = UserPost(identifier: "",
+                                postType: .photo,
+                                thumbnailImage: URL(string:"https://www.google.com" )!,
+                                postURL: URL(string:"https://www.google.com" )!,
+                                caption: nil,
+                                likeCount: [],
+                                comments: [],
+                                createdDate: Date(),
+                                taggedUsers: [])
+            let model = UserNotification(type: x % 2 == 0 ? .like(post: post) : .follow,
+                                         text: "Hello World",
+                                         user: User(username: "joe",
+                                                    bio: "",
+                                                    name: (first: "", last: ""),
+                                                    profilePhoto: URL(string: "https://www.google.com")!,
+                                                    birthDate:Date(),
+                                                    gender: .male,
+                                                    count: UserCount(followers: 1, following: 1, posts: 1),
+                                                    joinDate: Date()))
+            models.append(model)
+        }
+    }
+    
     private func addNonotificationView() {
         view.isHidden = true
         view.addSubview(noNotificationView)
@@ -65,13 +93,26 @@ final class NotificationViewController: UIViewController,UITableViewDelegate,UIT
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return models.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",for:indexPath)
-        return cell
+        let model = models[indexPath.row]
+        switch model.type {
+        case .like(_):
+            //likeCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: NotificationLikeEventTableViewCell.identifier,for:indexPath) as! NotificationLikeEventTableViewCell
+            cell.configure(with:model)
+            return cell
+        case .follow:
+            //follow cell
+            let cell = tableView.dequeueReusableCell(withIdentifier: NotificationFollowEventTableViewCell.identifier,for:indexPath) as! NotificationFollowEventTableViewCell
+            //cell.configure(with:model)
+            return cell
+        }
     }
     
-  
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
 }
--- GeekBlog/GeekBlog/Views/Cells/Notifications/NotificationLikeEventTableViewCell.swift
+++ GeekBlog/GeekBlog/Views/Cells/Notifications/NotificationLikeEventTableViewCell.swift
@@ -5,6 +5,7 @@
 //  Created by sangheon on 2022/01/23.
 //
 
+import SDWebImage
 import UIKit
 import SnapKit
 
@@ -13,7 +14,7 @@ protocol NotificationLikeEventTableViewCellDelegate:AnyObject {
 }
 
 class NotificationLikeEventTableViewCell: UITableViewCell {
-    static let identifier = "NotificationFollowEventTableViewCell"
+    static let identifier = "NotificationLikeEventTableViewCell"
     
     weak var delegate:NotificationFollowEventTableViewCellDelegate?
     
@@ -21,7 +22,9 @@ class NotificationLikeEventTableViewCell: UITableViewCell {
     
     private let profileImageView:UIImageView = {
         let imageView = UIImageView()
+        imageView.backgroundColor = .tertiarySystemBackground
         imageView.layer.masksToBounds = true
+        imageView.image = UIImage(named: "test")
         imageView.contentMode = .scaleAspectFill
         return imageView
     }()
@@ -30,11 +33,13 @@ class NotificationLikeEventTableViewCell: UITableViewCell {
         let label = UILabel()
         label.textColor = .label
         label.numberOfLines = 0
+        label.text = "@joe liked your Photo"
         return label
     }()
     
     private let postButton:UIButton = {
         let button = UIButton()
+        button.setBackgroundImage(UIImage(named: "test"), for: .normal)
         return button
     }()
     
@@ -44,7 +49,8 @@ class NotificationLikeEventTableViewCell: UITableViewCell {
         contentView.addSubview(profileImageView)
         contentView.addSubview(label)
         contentView.addSubview(postButton)
-        
+        configureUI()
+        layoutIfNeeded()
     }
     
     required init?(coder: NSCoder) {
@@ -52,9 +58,22 @@ class NotificationLikeEventTableViewCell: UITableViewCell {
     }
     
     public func configure(with model:UserNotification) {
+        self.model = model
         
+        guard
+        
+        switch model.type {
+        case .like(let post):
+            let thumbnail = post.thumbnailImage
+            postButton.sd_setBackgroundImage(with: thumbnail, for: .normal, completed: nil)
+            
+        case .follow:
+            break
+        }
+        label.text = model.text
+        profileImageView.sd_setImage(with: model.user.profilePhoto, completed: nil)
     }
-    
+
     override func prepareForReuse() {
         super.prepareForReuse()
         postButton.setBackgroundImage(nil, for: .normal)
@@ -64,7 +83,35 @@ class NotificationLikeEventTableViewCell: UITableViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-       
+        profileImageView.layer.cornerRadius = profileImageView.height / 2.0
     }
-
+    
+    
+    //MARK: Configure
+    private func configureUI() {
+        //photo,text, post Button
+        profileImageView.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(3)
+            make.top.equalToSuperview().offset(3)
+            make.height.equalToSuperview().offset(-6)
+            make.width.equalTo(profileImageView.snp.height)
+        }
+        
+        let size = contentView.height-4
+        postButton.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(contentView.width-size)
+            make.top.equalToSuperview().offset(2)
+            make.width.equalTo(size)
+            make.height.equalTo(size)
+        }
+        
+        label.snp.makeConstraints { make in
+            make.left.equalTo(profileImageView.snp.right)
+            make.top.equalToSuperview()
+            make.right.equalToSuperview().offset(-20)
+            make.height.equalToSuperview()
+        }
+    }
 }
+
+
