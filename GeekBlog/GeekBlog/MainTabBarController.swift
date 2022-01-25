274a722a527a4220b572a741911e593799438c54
sanghun0724 <sanghun0724@naver.com>
2022-01-25 19:56:12 +0900

    branch change

---
+++ GeekBlog/GeekBlog/MainTabBarController.swift
@@ -0,0 +1,28 @@
+//
+//  MainTabBarController.swift
+//  GeekBlog
+//
+//  Created by sangheon on 2022/01/25.
+//
+
+import UIKit
+
+class MainTabBarController:UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.tintColor = .black
+        tabBar.isTranslucent =  false
+        
+    }
+    
+    private func setupViewController() {
+        
+    }
+    
+    
+    private func templateNavcontroller(unselectedImage:UIImage,selectedImage:UIImage) {
+        
+    }
+    
+}
