49581b9d9e9638b6795e0d6d3965cd4d4745348d
sanghun0724 <sanghun0724@naver.com>
2022-01-26 14:44:36 +0900

    set controllers

---
+++ Thoughts/Thoughts/Controllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thoughts
+//
+//  Created by sangheon on 2022/01/26.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .link
+    }
+
+
+}
+
---
+++ Thoughts/Thoughts/Controllers/SignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewController.swift
+//  Thoughts
+//
+//  Created by sangheon on 2022/01/26.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
